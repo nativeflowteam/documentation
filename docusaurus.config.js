5c1bd8b9bc78d964c92df259c9ecc1bd98c5f356
Jay Singh <140599484+mathdebate09@users.noreply.github.com>
2024-09-01 23:08:10 +0530

    chore: init cname

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: "img/favicon.svg",
 
   // Set the production url of your site here
-  url: "",
+  url: "https://nativeflow.js.org",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/documentation/",
