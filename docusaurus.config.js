8af31a423465595a2157ab2a1fa9f2417ae9d7c5
Jay Singh <140599484+mathdebate09@users.noreply.github.com>
2024-09-22 15:17:28 +0530

    feat: twitter og content

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -77,27 +77,55 @@ const config = {
       metadata: [
         {
           name: "keywords",
-          content:
-            "react native, react native expo, react native paper, react native cli, react native tutorial, react native stylesheet, react native navigation, react native app, react native animations, react native android, react native alternatives, react native action sheet, animated react native, android studio react native, react native box shadow, react native border, react native button style, react native button style, react native background image, box shadow react native, background image react native, best react native ui library, border react native, react native config, react native docs, react native elements, react native expo tutorial, react native elevation, expo vs react native, expo go react native, react native flex, react native frameworks, react native fs, flex react native, github react native, react native flow, nativeflow, nativeflowcss, nativeflow css, native flow, justifycontent react native, javascript reactjs, mern stack, reactjs tutorial, react and redux, reduxs, react with redux, css grid css, react js redux, react js with redux, react redux js, reactjs native, redux for react, progressive web app, react component library, pwa app, storybooks, next js template, git tutorial for beginners, documentation maker, pwa progressive web app, table in react, component library react, react js table, react js charts, chart in react js, mern stack project, reactjs charts, charts for react, react site, chart react js, react js component library, reactjs component library, react js graph, graphs in react, react for website, carousel react js, react native play video, css layout grid, react js carousel, carousel for react, carousel in react js, carousel reactjs, chart js in react, chart reactjs, charts with react, create nextapp, css grid display, css minification, d3 data driven documents, daisy ui, datepicker in react, documentation node js, dropzone react js, formvalidation js, generate json from json schema, generate json from schema, git tutor, html form with javascript validation, html minification, javascript minification, javascript minified, js minification, json generate schema, json generator from schema, json schema for json schema, json schema from json, json schema generator from json, json to json schema generator, json to pretty, json to schema generator, json with schema, leaflet js react, minification html, module node js, next authjs, nuclear music app, play video in react native, react calendar picker, react date pickers, react date selector, react daterange, react js and redux, react js chart js, react js date picker, react js grid layout, react js tooltip, react native and redux, react native with redux, react what is redux, reactjs carousel, reactjs graph, reactjs redux toolkit, reactjs tooltip, reactjsorg, redux and react native, redux js tutorial, redux on react native, redux sagas, redux with react js, saga redux",
+          content: "react native, react native expo, react native paper, react native cli, react native tutorial, react native stylesheet, react native navigation, react native app, react native animations, react native android, react native alternatives, react native action sheet, animated react native, android studio react native, react native box shadow, react native border, react native button style, react native button style, react native background image, box shadow react native, background image react native, best react native ui library, border react native, react native config, react native docs, react native elements, react native expo tutorial, react native elevation, expo vs react native, expo go react native, react native flex, react native frameworks, react native fs, flex react native, github react native, react native flow, nativeflow, nativeflowcss, nativeflow css, native flow, justifycontent react native, javascript reactjs, mern stack, reactjs tutorial, react and redux, reduxs, react with redux, css grid css, react js redux, react js with redux, react redux js, reactjs native, redux for react, progressive web app, react component library, pwa app, storybooks, next js template, git tutorial for beginners, documentation maker, pwa progressive web app, table in react, component library react, react js table, react js charts, chart in react js, mern stack project, reactjs charts, charts for react, react site, chart react js, react js component library, reactjs component library, react js graph, graphs in react, react for website, carousel react js, react native play video, css layout grid, react js carousel, carousel for react, carousel in react js, carousel reactjs, chart js in react, chart reactjs, charts with react, create nextapp, css grid display, css minification, d3 data driven documents, daisy ui, datepicker in react, documentation node js, dropzone react js, formvalidation js, generate json from json schema, generate json from schema, git tutor, html form with javascript validation, html minification, javascript minification, javascript minified, js minification, json generate schema, json generator from schema, json schema for json schema, json schema from json, json schema generator from json, json to json schema generator, json to pretty, json to schema generator, json with schema, leaflet js react, minification html, module node js, next authjs, nuclear music app, play video in react native, react calendar picker, react date pickers, react date selector, react daterange, react js and redux, react js chart js, react js date picker, react js grid layout, react js tooltip, react native and redux, react native with redux, react what is redux, reactjs carousel, reactjs graph, reactjs redux toolkit, reactjs tooltip, reactjsorg, redux and react native, redux js tutorial, redux on react native, redux sagas, redux with react js, saga redux"
         },
         {
-          name: "og:image",
-          content:
-            "https://mathdebate09.github.io/dummy-img-cdn/assets/nativeflow/meta.png",
+          name: "twitter:image:src",
+          content: "https://mathdebate09.github.io/dummy-img-cdn/assets/nativeflow/twitter.png"
         },
-        { name: "og:type", content: "website" },
-        { name: "og:image:type", content: "image/png" },
-        { name: "og:image:width", content: "256" },
-        { name: "og:image:height", content: "256" },
         {
-          name: "twitter:image",
-          content:
-            "https://mathdebate09.github.io/dummy-img-cdn/assets/nativeflow/twitter.png",
+          name: "twitter:site",
+          content: "@jayowiee"
         },
         {
           name: "twitter:card",
-          content:
-            "https://mathdebate09.github.io/dummy-img-cdn/assets/nativeflow/twitter.png",
+          content: "sumarry_large_image"
+        },
+        {
+          name: "twitter:title",
+          content: "NativeFlow - Styling library for React Native"
+        },
+        {
+          name: "twitter:description",
+          content: "Built to stay closer to ecosystem so that things break less often"
+        },
+        {
+          name: "og:image",
+          content: "https://mathdebate09.github.io/dummy-img-cdn/assets/nativeflow/meta.png",
+        },
+        {
+          name: "og:image:alt",
+          content: "NativeFlow, style React Native. Faster. Better."
+        },
+        {
+          name: "og:image:width",
+          content: "791"
+        },
+        {
+          name: "og:image:height",
+          content: "446"
+        },
+        {
+          name: "og:site_name",
+          content: "NativeFlow"
+        },
+        {
+          name: "og:type",
+          content: "object"
+        },
+        {
+          name: "url",
+          content: "https://nativeflow.js.org"
         },
       ],
       navbar: {
